b95152816da7f21a63895d8ee76b15c17c6325f2
tarushm2006 <69667062+tarushm2006@users.noreply.github.com>
2023-06-17 17:12:15 +0530

    Added api file

--- src/index.js
+++ src/index.js
@@ -4,24 +4,26 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import Store from "./redux/store";
-import { configureChains, WagmiConfig, createClient } from "wagmi"
-import {polygonMumbai} from "wagmi/chains";
-import {publicProvider } from "wagmi/providers/public";
+import { configureChains, WagmiConfig, createClient } from "wagmi";
+import { polygonMumbai } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
 // import { createClient } from 'wagmi';
-const { chains,provider,webSocketProvider } = configureChains([polygonMumbai],[publicProvider()],);
+const { provider, webSocketProvider } = configureChains(
+  [polygonMumbai],
+  [publicProvider()]
+);
 
 const client = createClient({
   autoConnect: true,
   provider,
-  webSocketProvider
-})
-
+  webSocketProvider,
+});
 
 ReactDOM.render(
   <WagmiConfig client={client}>
-  <Provider store={Store}>
-    <App />
-  </Provider>
+    <Provider store={Store}>
+      <App />
+    </Provider>
   </WagmiConfig>,
   document.getElementById("root")
 );
